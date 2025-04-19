7edeb8d0f176e3168c6bfabae224b1c22ca6f602
Alexis-NM <alexis.na.me@gmail.com>
2025-04-19 15:33:29 +0200

    Updates

--- src/api/jokeApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_BASE = import.meta.env.VITE_API_BASE;
-
-export const fetchRandomJoke = () =>
-  axios.get(`${API_BASE}/jokes/random`).then(res => res.data);
-
-export const fetchAllJokes = () =>
-  axios.get(`${API_BASE}/jokes`).then(res => res.data);
--- src/components/JokeDisplay.jsx
+++
@@ -1,7 +0,0 @@
-export function JokeDisplay({ content }) {
-  return (
-    <div className="joke">
-      {content || "Clique sur le bouton pour une blague"}
-    </div>
-  );
-}
